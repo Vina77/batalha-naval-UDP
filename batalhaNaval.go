6582a05280b09857eb06d81fd8c4f7c491e3f473
Vina77 <marcus300702@outlook.com>
2024-04-29 16:33:09 -0300

    Utilização de UDP

---
+++ batalhaNaval.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+
+	addr, err := net.ResolveUDPAddr("udp", "127.0.0.1:8080")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	conn, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	// Enviar mensagem
+	message := []byte("hello")
+	_, err = conn.Write(message)
+
+	// Receber mensagem
+	buffer := make([]byte, 1024)
+	n, _, err := conn.ReadFromUDP(buffer)
+	fmt.Println(string(buffer[:n]))
+
+}
