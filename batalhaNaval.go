0b9ddac9c9ccf0c486f0d4ec5ec29d30f6dd3f0c
Vina77 <marcus300702@outlook.com>
2024-04-29 17:30:26 -0300

    Tabuleiro e Navios

--- batalhaNaval.go
+++ batalhaNaval.go
@@ -6,6 +6,16 @@ import (
 	"os"
 )
 
+type Ship struct {
+	Name string
+	Size int8
+}
+
+type Board struct {
+	Grid  [10][10]string
+	Ships []Ship
+}
+
 func main() {
 
 	addr, err := net.ResolveUDPAddr("udp", "127.0.0.1:8080")
